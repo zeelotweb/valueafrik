c233b854ce2c101f0afb7736fcce96844a17bb50
zeelotweb <67663419+zeelotweb@users.noreply.github.com>
2026-09-14 07:56:28 -0400

    update

--- resources/views/partials/marketing-header.blade.php
+++ resources/views/partials/marketing-header.blade.php
@@ -11,6 +11,24 @@
                 <a href="{{ url('/dashboard') }}" title="Dashboard" class="btn-flat-primary flex size-9 items-center justify-center rounded-md">
                     <flux:icon.home class="size-5" />
                 </a>
+                <a
+                    href="{{ route('profile.edit') }}"
+                    wire:navigate
+                    title="Settings"
+                    class="flex size-9 items-center justify-center rounded-md text-stone-500 hover:bg-stone-200 hover:text-stone-900 dark:text-stone-400 dark:hover:bg-stone-800 dark:hover:text-white"
+                >
+                    <flux:icon.cog-6-tooth class="size-5" />
+                </a>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button
+                        type="submit"
+                        title="Log out"
+                        class="flex size-9 items-center justify-center rounded-md text-stone-500 hover:bg-stone-200 hover:text-stone-900 dark:text-stone-400 dark:hover:bg-stone-800 dark:hover:text-white"
+                    >
+                        <flux:icon.arrow-right-start-on-rectangle class="size-5" />
+                    </button>
+                </form>
             @else
                 <a href="{{ route('login') }}" class="text-stone-600 hover:text-stone-900 dark:text-stone-400 dark:hover:text-white">
                     Log in
